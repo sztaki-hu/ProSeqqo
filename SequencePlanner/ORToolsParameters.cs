362b776fb6f0fb245bf5700c34db040748e6503a
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-06-27 19:17:18 +0200

    OR-Tools feature

---
+++ SequencePlanner/ORToolsParameters.cs
@@ -0,0 +1,16 @@
+﻿using SequencePlanner.GTSP;
+using SequencePlanner.Phraser.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner
+{
+    public class ORToolsParameters
+    {
+        public GTSPRepresentation GTSP { get; set; }
+        public Position StartDepot { get; set; }
+        public int Timelimit { get; set; }
+        public int WeightMultiplier { get; set; }
+    }
+}
---
+++ SequencePlanner/ORToolsResult.cs
@@ -0,0 +1,51 @@
+﻿using SequencePlanner.GTSP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner
+{
+    public class ORToolsResult
+    {
+        public List<Position> Solution { get; set; }
+        public double[] Costs { get; set; }
+        public double CostSum { get; set; }
+
+        public void ResolveSolution(int[] solution, GTSPRepresentation gtsp)
+        {
+            foreach (var item in solution)
+            {
+                foreach (var position in gtsp.Positions)
+                {
+                    if(position.PID == item)
+                    {
+                        Solution.Add(position);
+                    }
+                }
+            }
+        }
+
+        public void WriteSimple()
+        {
+            for (int i = 0; i < Solution.Count; i++)
+            {
+                Console.Write("["+ Solution[i].ID+ "]"+ "[" + Solution[i].PID+ "]"+Solution[i].Name+"--"+Costs[i]+"-->");
+            }
+        }
+        public void Write()
+        {
+            for (int i = 0; i < Solution.Count; i++)
+            {
+                Console.WriteLine("[" + Solution[i].ID + "]" + "[" + Solution[i].PID + "]" + Solution[i].Name + Solution[i].Configuration);
+            }
+        }
+        public void WriteFull()
+        {
+            for (int i = 0; i < Solution.Count; i++)
+            {
+                Console.WriteLine("| [" + Solution[i].ID + "]" + "[" + Solution[i].PID + "]" + Solution[i].Name + Solution[i].Configuration);
+                Console.WriteLine("|- "+ Costs[i]);
+            }
+        }
+    }
+}
