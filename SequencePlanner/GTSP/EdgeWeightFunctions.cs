69b6aab4991e4a44a8d678ec7eba658ef2edd823
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-06-29 22:14:34 +0200

    Implement Trapezoid Time as EdgeWeightFunction

--- SequencePlanner/GTSP/EdgeWeightFunctions.cs
+++ SequencePlanner/GTSP/EdgeWeightFunctions.cs
@@ -8,6 +8,10 @@ namespace SequencePlanner.GTSP
     public class EdgeWeightFunctions
     {
         public delegate double EdgeWeightFunction(List<double> a, List<double> b, params object[] param);
+        public static double[] JointAcceleration;
+        public static double[] JointSpeed;
+        public static double[] JointThresholdTime;
+        public static double[] JointThresholdDist;
 
         public static double Euclidian_Distance(List<double> a, List<double> b, params object[] param)
         {
@@ -48,12 +52,58 @@ namespace SequencePlanner.GTSP
             }
         }
 
+        public static void setTrapezoidParam(double[] maxAcc, double[] maxSpeed)
+        {
+            JointAcceleration = maxAcc;
+            JointSpeed = maxSpeed;
+            int Dimension = maxAcc.Length;
+            JointThresholdTime = new double[Dimension];
+            JointThresholdDist = new double[Dimension];
+
+            if (JointAcceleration.Length == Dimension && JointSpeed.Length == Dimension)
+            {
+                for (int i = 0; i < Dimension; i++)
+                {
+                    if (JointAcceleration[i] == 0.0)
+                    {
+                        throw new DivideByZeroException("Trapezoid_Time Error: Acceleration is zero!");
+                    }
+                    else
+                    {
+                        JointThresholdDist[i] = JointSpeed[i] * JointSpeed[i] / JointAcceleration[i];
+                        JointThresholdTime[i] = Math.Sqrt(4.0 * JointThresholdDist[i] / JointAcceleration[i]);
+                    }
+                }
+            }
+        }
+
+
         public static double Trapezoid_Time(List<double> a, List<double> b, params object[] param)
         {
-            if (a.Count == b.Count)
-            {
-                Console.WriteLine("EdgeWeightFunctions:Trapezoid_Time not implemented yet!");
-                return 0;
+            int Dimension = a.Count;
+
+            if (b.Count == Dimension && JointAcceleration.Length == Dimension && JointSpeed.Length == Dimension)
+            { 
+                double maxTime = 0;
+                double sumTime = 0;
+                // Max of joint travel times
+                for (int i = 0; i < Dimension; i++)
+                {
+                    double angleDiff = Math.Abs(a[i] - b[i]);
+                    // This version is for the 2*PI simmetry
+                    //double angleDiff = min(abs(c1.x[i] - c2.x[i]), min(abs(c1.x[i] - c2.x[i] + TWOPI), abs(c1.x[i] - c2.x[i] - TWOPI)));
+                    double time;
+                    if (angleDiff < JointThresholdDist[i])
+                        // Triangle profile
+                        time = Math.Sqrt(4.0 * angleDiff / JointAcceleration[i]);
+                    else
+                        // Trapezoid profile
+                        time = JointThresholdTime[i] + (angleDiff - JointThresholdDist[i]) / JointSpeed[i];
+                    if (maxTime < time)
+                        maxTime = time;
+                    sumTime += time;
+                }
+                return maxTime;// + 0.01 * (sumTime / Dimension);
             }
             else
             {
