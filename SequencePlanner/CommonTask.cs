54888351bc3b09994ee6e3ca8e8a2bd28751b96b
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-07-10 17:45:14 +0200

    Add Common/LineLike/PointLike Tasks

---
+++ SequencePlanner/CommonTask.cs
@@ -0,0 +1,30 @@
+﻿using SequencePlanner.Phraser.Template;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner
+{
+    public class CommonTask : GTSPTask
+    {
+        public override void Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ORToolsResult Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Templateing(Template template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
