e70b1fe746ad7559d1bd09683f9c492b4d36838d
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-07-10 17:43:29 +0200

    Add CommonTemplate

---
+++ SequencePlanner/Phraser/Template/CommonTemplateCompiler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner.Phraser.Template
+{
+    public class CommonTemplateCompiler : TemplateCompiler
+    {
+        public CommonTask Compile(Template template)
+        {
+            return new CommonTask();
+        }
+    }
+}
---
+++ SequencePlanner/Phraser/Template/CommonTemplateValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner.Phraser.Template
+{
+    public class CommonTemplateValidator : TemplateValidator
+    {
+        public override bool Validate(Template t)
+        {
+            return true;
+        }
+    }
+}
