70e0867af3882de99d206302022164a821e34af2
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-07-11 16:49:07 +0200

    Add Template Manager

--- SequencePlanner/CommonTask.cs
+++ SequencePlanner/CommonTask.cs
@@ -1,30 +1,24 @@
-﻿using SequencePlanner.Phraser.Template;
+﻿using SequencePlanner.GTSP;
+using SequencePlanner.Phraser.Options.Values;
+using SequencePlanner.Phraser.Template;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SequencePlanner
 {
-    public class CommonTask : GTSPTask
+    public class CommonTask
     {
-        public override void Build()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override ORToolsResult Run()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Templateing(Template template)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Validate()
-        {
-            throw new NotImplementedException();
-        }
+        public TaskTypeEnum TaskType { get; set; }
+        public EdgeWeightSourceEnum EdgeWeightSource { get; set; }
+        public EdgeWeightCalculator DistanceFunction { get; set; }
+        public int Dimension { get; set; }
+        public int TimeLimit { get; set; }
+        public bool CyclicSequence { get; set; }
+        public Position StartDepot { get; set; }
+        public Position FinishDepot { get; set; }
+        public int WeightMultiplier { get; set; }
+        public List<Position> PositionList { get; set; }
+        public PositionMatrixOptionValue PositionMatrix { get; set; }
     }
 }
