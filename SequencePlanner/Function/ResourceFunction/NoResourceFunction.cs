7ec61df5970aa625bad8c8629e931dfa16e8f605
Zahorán László <zahoran.laszlo@sztaki.hu>
2020-12-07 04:29:35 +0100

    Small fixes

--- SequencePlanner/Function/ResourceFunction/NoResourceFunction.cs
+++ SequencePlanner/Function/ResourceFunction/NoResourceFunction.cs
@@ -6,17 +6,9 @@ namespace SequencePlanner.Function.ResourceFunction
     public class NoResourceFunction : IResourceFunction
     {
         public string FunctionName { get { return "NoResource"; } }
-        public double Cost { get; }
         public IResourceDistanceLinkFunction LinkingFunction { get; set; }
 
-        public double ComputeResourceCost(Position A, Position B, double distance)
-        {
-            return distance;
-        }
-
-        public void Validate()
-        {
-            
-        }
+        public double ComputeResourceCost(Position A, Position B, double distance) => distance;
+        public void Validate(){}
     }
 }
