dad83315cc681ad43b67723dc1372768ac284521
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-02-02 22:48:37 +0100

    Create DistanceFunction abstract class, add StrictUserEdgeWeights and StrictSystemEdgeWeights

---
+++ SequencePlanner/Function/DistanceFunction/DistanceFunction.cs
@@ -0,0 +1,35 @@
+﻿using SequencePlanner.Helper;
+using SequencePlanner.Model;
+
+namespace SequencePlanner.Function.DistanceFunction
+{
+    public abstract class DistanceFunction: IDistanceFunction
+    {
+        public string FunctionName { get; protected set; }
+        public StrictEdgeWeightSet StrictUserEdgeWeights {get;set;}
+        public StrictEdgeWeightSet StrictSystemEdgeWeights {get;set;}
+
+        public DistanceFunction()
+        {
+            StrictUserEdgeWeights = new StrictEdgeWeightSet();
+            StrictSystemEdgeWeights = new StrictEdgeWeightSet();
+        }
+        public abstract double ComputeDistance(Position A, Position B);
+        public abstract void Validate();
+        public StrictEdgeWeight GetStrictEdgeWeight(Position A, Position B)
+        {
+            var user = StrictUserEdgeWeights.Get(A, B);
+            var system = StrictSystemEdgeWeights.Get(A, B);
+            if (system != null)
+            {
+                SeqLogger.Warning("System generated edge weight ovveride user given, between positions with " + A.GlobalID + ", " + B.GlobalID + "global id.", nameof(DistanceFunction));
+                return system;
+            }
+            if (user != null)
+            {
+                return user;
+            }
+            return null;
+        }
+    }
+}
--- SequencePlanner/Function/DistanceFunction/IDistanceFunction.cs
+++ SequencePlanner/Function/DistanceFunction/IDistanceFunction.cs
@@ -1,10 +1,13 @@
 ﻿using SequencePlanner.Model;
+using System.Collections.Generic;
 
 namespace SequencePlanner.Function.DistanceFunction
 {
     public interface IDistanceFunction
     {
         public string FunctionName { get; }
+        public StrictEdgeWeightSet StrictUserEdgeWeights {get;set;}
+        public StrictEdgeWeightSet StrictSystemEdgeWeights {get;set;}
         public double ComputeDistance(Position A, Position B);
         public void Validate();
     }
