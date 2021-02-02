58815ea6c9ece39a22c4a18d56fd12a088cb508f
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-02-02 22:42:05 +0100

    Add StrictEdgeWeight and set

---
+++ SequencePlanner/Model/StrictEdgeWeight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner.Model
+{
+    public class StrictEdgeWeight
+    {
+        public Position A { get; set; }
+        public Position B { get; set; }
+        public double Weight { get; set; }
+        public bool Bidirectional { get; set; }
+
+        public StrictEdgeWeight(Position a, Position b, double weight, bool bidirectional = false)
+        {
+            A = a;
+            B = b;
+            Weight = weight;
+            Bidirectional = bidirectional;
+        }
+
+        public bool FitFor(BaseNode a, BaseNode b)
+        {
+            if (Bidirectional)
+                return ((this.A.GlobalID == a.GlobalID && B.GlobalID == b.GlobalID) || (this.A.GlobalID == b.GlobalID && B.GlobalID == a.GlobalID));
+            else
+                return (this.A.GlobalID == a.GlobalID && B.GlobalID == b.GlobalID);
+        }
+    }
+}
---
+++ SequencePlanner/Model/StrictEdgeWeightSet.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencePlanner.Model
+{
+    public class StrictEdgeWeightSet
+    {
+        private List<StrictEdgeWeight> List { get; set; }
+
+        public StrictEdgeWeightSet()
+        {
+            List = new List<StrictEdgeWeight>();
+        }
+
+        public StrictEdgeWeightSet(List<StrictEdgeWeight> strictEdges)
+        {
+            List = strictEdges;
+        }
+
+        public List<StrictEdgeWeight> GetAll()
+        {
+            return List;
+        }
+            
+        public List<StrictEdgeWeight> Get(Position position)
+        {
+            var tmp = new List<StrictEdgeWeight>();
+            foreach (var item in List)
+            {
+                if ((item.A.GlobalID == position.GlobalID) || (item.B.GlobalID == position.GlobalID))
+                    tmp.Add(item);
+            }
+            if (tmp.Count == 0)
+                return null;
+            else
+                return tmp;
+        }
+        public StrictEdgeWeight Get(BaseNode node1, BaseNode node2)
+        {
+            foreach (var item in List)
+            {
+                if ((item.A.GlobalID == node1.GlobalID && item.B.GlobalID == node2.GlobalID) || (item.A.GlobalID == node2.GlobalID && item.B.GlobalID == node1.GlobalID))
+                    return item;
+            }
+            return null;
+        }
+
+        public void Add(Position a, Position b, double weight)
+        {
+            List.Add(new StrictEdgeWeight(a,b,weight));
+        }
+
+        public void Add(StrictEdgeWeight strictEdge)
+        {
+            List.Add(strictEdge);
+        }
+
+        public void Delete(Position A, Position B)
+        {
+            List.Remove(Get(A, (BaseNode)B));
+        }
+
+        //Delete edges that contains the given position
+        public void Delete(Position position){
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].A.GlobalID == position.GlobalID || List[i].B.GlobalID == position.GlobalID)
+                {
+                    List.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+    }
+}
