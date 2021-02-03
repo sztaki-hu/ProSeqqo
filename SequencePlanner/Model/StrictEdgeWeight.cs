521e05978f2f17a0703a154e8d0c40a87c109dd8
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-02-03 22:37:10 +0100

    Position to BaseNode in StrictEdgeWeight

--- SequencePlanner/Model/StrictEdgeWeight.cs
+++ SequencePlanner/Model/StrictEdgeWeight.cs
@@ -6,12 +6,12 @@ namespace SequencePlanner.Model
 {
     public class StrictEdgeWeight
     {
-        public Position A { get; set; }
-        public Position B { get; set; }
+        public BaseNode A { get; set; }
+        public BaseNode B { get; set; }
         public double Weight { get; set; }
         public bool Bidirectional { get; set; }
 
-        public StrictEdgeWeight(Position a, Position b, double weight, bool bidirectional = false)
+        public StrictEdgeWeight(BaseNode a, BaseNode b, double weight, bool bidirectional = false)
         {
             A = a;
             B = b;
