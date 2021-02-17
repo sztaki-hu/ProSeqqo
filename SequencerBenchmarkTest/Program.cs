54edba5eade7e05ba23dd1f80c0e2c8d1a100278
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-02-17 20:03:39 +0100

    Add SequencerBenchmarkTest Console Application

---
+++ SequencerBenchmarkTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using SequencerTest.Benchmark;
+using System;
+
+namespace SequencerBenchmarkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Init();
+            runner.RunBenchmark();
+        }
+    }
+}
