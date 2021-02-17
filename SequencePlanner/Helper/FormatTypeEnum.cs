18810e9d25278c1eeeda2d3de640d425bff0a587
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-02-17 20:01:24 +0100

    Add Enums to helper

---
+++ SequencePlanner/Helper/FormatTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequencePlanner.Helper
+{
+    public enum FormatType
+    {
+        JSON,
+        XML,
+        TXT,
+        SEQ,
+        Unknown
+    }
+}
+
