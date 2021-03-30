e4feb0918d88970fd9c254388679352be575013e
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-03-30 12:51:06 +0200

    Move common validators to BaseTaskValidator

--- SequencePlanner/GTSPTask/Task/PointLike/PointLikeTaskValidator.cs
+++ SequencePlanner/GTSPTask/Task/PointLike/PointLikeTaskValidator.cs
@@ -1,4 +1,5 @@
 ﻿using SequencePlanner.GTSP;
+using SequencePlanner.GTSPTask.Task.Base;
 using SequencePlanner.GTSPTask.Task.PointLike;
 using SequencePlanner.Helper;
 using SequencePlanner.Model;
@@ -19,23 +20,13 @@ namespace SequencePlanner.GTSPTask.Task.LineLike
         {
             SeqLogger.Info("Validation started!", nameof(PointLikeTaskValidator));
             SeqLogger.Indent++;
+            BaseTaskValidator baseTaskValidator = new BaseTaskValidator();
+            baseTaskValidator.Validate((BaseTask)pointLikeTask);
             CheckTaskType(pointLikeTask);
-            if (pointLikeTask.PositionMatrix.DistanceFunction.FunctionName!="MatrixDistance")
-                CheckDimension(pointLikeTask.Dimension);
-            CheckDistaceFunction(pointLikeTask);
-            CheckWeightMultiplier(pointLikeTask);
             CheckStartFinsihDepot(pointLikeTask);
-            CheckTimeLimit(pointLikeTask);
-            CheckMIPpresolver(pointLikeTask);
-            CheckResourceFunction(pointLikeTask);
-            CheckLocalSearchStrategy(pointLikeTask);
-            CheckPositionList(pointLikeTask);
-            CheckPositionMatrix(pointLikeTask.PositionMatrix);
-            CheckStrictEdgeWeights(pointLikeTask);
             CheckProcessHierarchy(pointLikeTask);
             CheckProcessPrecedence(pointLikeTask);
             CheckUseAlternativeShortcuts(pointLikeTask);
-            CheckCycle(pointLikeTask.CyclicSequence, pointLikeTask.StartDepot, pointLikeTask.FinishDepot);
             SeqLogger.Indent--;
             SeqLogger.Info("Validation finished!", nameof(PointLikeTaskValidator));
         }
@@ -148,107 +139,6 @@ namespace SequencePlanner.GTSPTask.Task.LineLike
             }
         }
 
-        //Q13
-        private void CheckStrictEdgeWeights(PointLikeTask task)
-        {
-            if (task.PositionMatrix.DistanceFunction.StrictUserEdgeWeights is null)
-                throw new SeqException("PositionMatrix.DistanceFunction.StrictUserEdgeWeights are not initialized.");
-            foreach (var weights in task.PositionMatrix.DistanceFunction.StrictUserEdgeWeights.GetAll())
-            {
-                var findA = false;
-                var findB = false;
-                foreach (var position in task.PositionMatrix.Positions)
-                {
-                    if (weights.A.GlobalID == position.Node.GlobalID)
-                        findA = true;
-                    if (weights.B.GlobalID == position.Node.GlobalID)
-                        findB = true;
-                }
-                if(!findA)
-                    throw new SeqException("PositionMatrix.DistanceFunction.StrictUserEdgeWeights contains position with wrong userID: "+ weights.A.GlobalID);
-
-                if(!findB)
-                    throw new SeqException("PositionMatrix.DistanceFunction.StrictUserEdgeWeights contains position with wrong userID: "+ weights.B.GlobalID);
-            }
-        }
-
-        //Q12
-        private void CheckPositionMatrix(PositionMatrix positionMatrix)
-        {
-            if (positionMatrix == null)
-                throw new SeqException("PositionMatrix.PositionMatrix not given.");
-            else
-            {
-                positionMatrix.Validate();
-            }
-            SeqLogger.Trace("CheckPositionMatrix validated!", nameof(PointLikeTaskValidator));
-        }
-
-        //Q11
-        private void CheckPositionList(PointLikeTask task)
-        {
-            if (task.PositionMatrix is null)
-                throw new SeqException("Position hierarchy, PositionMatrix is null.", "Please construct it.");
-            if (task.PositionMatrix.Positions is null)
-                throw new SeqException("Position matrix is null.", "Please construct it.");
-            if (task.PositionMatrix.Positions.Count < 1)
-                throw new SeqException("PositionMatrix.Positions not contain positions.", "Please add them.");
-            var posList = task.PositionMatrix.Positions;
-            for (int i = 0; i < posList.Count; i++)
-            {
-                for (int j = 0; j < posList.Count; j++)
-                {
-                    if (i != j)
-                    {
-                        if (posList[i].Node.GlobalID == posList[j].Node.GlobalID)
-                            throw new SeqException("PositionMatrix.Positions contains position multiple times with GlobalID: " + posList[i].Node.GlobalID, "Remove duplicated positions.");
-                        if (posList[i].Node.UserID == posList[j].Node.UserID)
-                            throw new SeqException("PositionMatrix.Positions contains position multiple times with UserID: " + posList[i].Node.UserID, "Remove duplicated positions.");
-                        //if (posList[i].Node.SequencingID == posList[j].Node.SequencingID)
-                        //    throw new SeqException("PositionMatrix.Positions contains position multiple times times with SequencingID: " + posList[i].Node.SequencingID, "Remove duplicated positions.");
-                    }
-                }
-                if(posList[i].In.Vector.Length != task.Dimension || posList[i].In.Vector.Length != posList[i].Out.Vector.Length)
-                    throw new SeqException("Position with UserID: " + posList[i].In.UserID + " has dimension mismatch.");
-            }
-            SeqLogger.Info("PositionList: " + task.PositionMatrix.Positions.Count, nameof(PointLikeTaskValidator));
-        }
-
-        //Q10
-        private void CheckLocalSearchStrategy(PointLikeTask task)
-        {
-            SeqLogger.Info("LocalSearchStrategy: " + task.LocalSearchStrategy.ToString(), nameof(PointLikeTaskValidator));
-        }
-
-        //O9
-        private void CheckResourceFunction(PointLikeTask task)
-        {
-            if (task.PositionMatrix.ResourceFunction is null)
-                throw new SeqException("Resource function is null.", "Please construct it.");
-            task.PositionMatrix.ResourceFunction.Validate();
-        }
-
-        //O8
-        private void CheckMIPpresolver(PointLikeTask task)
-        {
-            SeqLogger.Info("UseMIPPrecedenceSolver: "+ task.UseMIPprecedenceSolver, nameof(PointLikeTaskValidator));
-        }
-
-        //O7
-        private void CheckTimeLimit(PointLikeTask task)
-        {
-            if (task.TimeLimit <= 0)
-            {
-                SeqLogger.Info("TimeLimit: 0 - Automatic based on solver", nameof(PointLikeTaskValidator));
-                SeqLogger.Warning("Time limit not given, running can take a long time", nameof(PointLikeTaskValidator));
-            }
-
-            if (task.TimeLimit > 0)
-                SeqLogger.Info("TimeLimit: "+task.TimeLimit+"ms - "+ System.TimeSpan.FromMilliseconds(task.TimeLimit).ToString(), nameof(PointLikeTaskValidator));
-            if(task.LocalSearchStrategy == OR_Tools.LocalSearchStrategyEnum.Metaheuristics.GuidedLocalSearch && task.TimeLimit<=0)
-                throw new SeqException("TimeLimit needed in case of "+ OR_Tools.LocalSearchStrategyEnum.Metaheuristics.GuidedLocalSearch.ToString() + " metaheuristic.");
-        }
-
         //O6
         private void CheckStartFinsihDepot(PointLikeTask task)
         {
@@ -262,23 +152,14 @@ namespace SequencePlanner.GTSPTask.Task.LineLike
             }
             else // not needed, optional
             {
-                if (task.StartDepot is null)
-                {
-                    SeqLogger.Info("StartDepot: -", nameof(PointLikeTaskValidator));
-                }
-                else
+                if (task.StartDepot is not null)
                 {
                     checkStartDepot = true;
-                    SeqLogger.Info("StartDepot: "+ task.FinishDepot.UserID, nameof(PointLikeTaskValidator));
+                    SeqLogger.Info("StartDepot: "+ task.StartDepot.UserID, nameof(PointLikeTaskValidator));
                 }
 
-                if (task.FinishDepot is null)
+                if (task.FinishDepot is  not null)
                 {
-                    SeqLogger.Info("FinshDepot: -", nameof(PointLikeTaskValidator));
-                }
-                else
-                {
-                    SeqLogger.Info("FinshDepot: "+ task.FinishDepot.UserID, nameof(PointLikeTaskValidator));
                     checkFinishDepot = true;
                 }
             }
@@ -354,43 +235,6 @@ namespace SequencePlanner.GTSPTask.Task.LineLike
                     throw new SeqException("StartDepot should contain by a process, alternative and a task.");
                 }
             }
-        }
-
-        //O5
-        private void CheckWeightMultiplier(PointLikeTask task)
-        {
-            if (task.WeightMultipier == 0)
-                throw new SeqException("WeightMultipier should be greater then 0.");
-            SeqLogger.Info("WeightMultipier: " + task.WeightMultipier, nameof(PointLikeTaskValidator));
-        }
-
-        //Q4
-        private void CheckCycle(bool cyclicSequence, Position startDepot, Position finishDepot)
-        {
-            if (cyclicSequence)
-            {
-                if (startDepot == null)
-                    throw new SeqException("In case of CyclicSequence StartDepot needed!");
-                if (finishDepot != null)
-                    throw new SeqException("In case of CyclicSequence FinishDepot not useable!");
-            }
-            SeqLogger.Trace("CheckCycle validated!", nameof(PointLikeTaskValidator));
-        }
-
-        //O3
-        private void CheckDistaceFunction(PointLikeTask task)
-        {
-            if(task.PositionMatrix.DistanceFunction is null)
-                throw new SeqException("Distance function is null.", "Please construct it.");
-            SeqLogger.Info("DistanceFunction:"+ task.PositionMatrix.DistanceFunction.FunctionName, nameof(PointLikeTaskValidator));
-            task.PositionMatrix.DistanceFunction.Validate();
-        }
-
-        //O2
-        private void CheckDimension(int dimension)
-        {
-            if (dimension <= 0)
-                throw new SeqException("Dimension should be > 0!");
         }
 
         //O1
