61e0f13e4934c39f17bd8996ea1b9eb32b179c06
Zahorán László <zahoran.laszlo@sztaki.hu>
2021-05-14 23:56:05 +0200

    Update Line Animation

--- LineAnimation/Line.cs
+++ LineAnimation/Line.cs
@@ -1,16 +1,30 @@
-﻿namespace LineAnimation
+﻿using SequencePlanner.Model.Hierarchy;
+
+namespace LineAnimation
 {
     public class Line
     {
-        public int ID { get; set; }
-        public int ContourID { get; set; }
-        public string Name { get; set; }
         public Point A { get; set; }
         public Point B { get; set; }
         public string Tag { get; set; }
         public DrawType DrawType { get; set; }
         public bool Visibility { get; set; }
-        public double Length { get; set; }
+
+        public Line(Motion m)
+        {
+            A = new Point(m.FirstConfig);
+            B = new Point(m.LastConfig);
+            Tag = m.ID+" "+m.Name;
+            DrawType = DrawType.Work;
+        }
+
+        public Line(Motion from, Motion to)
+        {
+            A = new Point(from.LastConfig);
+            B = new Point(to.FirstConfig);
+            //Tag = m.ID + " " + m.Name;
+            DrawType = DrawType.Travel;
+        }
     }
 
     public enum DrawType
